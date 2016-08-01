9f76294534b08d2d0e98a1336cd08d1ed6b6bff7
zhangkang1521 <zhangkang_pub@163.com>
2016-08-01 14:30:46 +0800

    oracle

---
+++ oracle导数据.sql
@@ -0,0 +1,21 @@
+select * from DBA_TABLESPACES
+
+-- 创建表空间
+CREATE TABLESPACE xxd 
+datafile 'E:\orcl\xxd.dbf' 
+size 1024m 
+autoextend on; 
+
+-- 创建用户
+CREATE USER xxd_v6_test 
+identified by xxd_v6_test 
+DEFAULT TABLESPACE xxd ; 
+
+-- 授权
+grant connect,resource,dba to xxd_v6_test; 
+
+select * from dba_users
+
+-- 导入导出数据
+EXP SYSTEM/123456@LOCALHOST/ORCL FILE=D:\SCOTT.DMP OWNER=(SCOTT)
+imp system/123456@localhost/ZK fromuser=xxd_v6_test touser=xxd_v6_test file=E:/v6_test.dmp
